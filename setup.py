a8aa63a41b6b17e869ce2ad59ba515b37763d594
qksolov <ekaterina.solov@mail.ru>
2025-05-11 19:55:32 +0300

    Update setup

--- setup.py
+++ setup.py
@@ -2,31 +2,38 @@ from setuptools import setup, find_packages
 
 setup(
     name="eyewear_counter",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "torch",
-        "torchvision",
-        "opencv-python",
+        "torchvision",        
         "ultralytics",
+        "opencv-python",
         "numpy",
         "pandas",
         "aiohttp",
         "tqdm",
-        "xlsxwriter"
+        "xlsxwriter",
+        "nest_asyncio",
+        "requests",
+        "gradio",
     ],
-    extras_require={
-        "app": [
-            "gradio",
-            "nest_asyncio",
-        ],
-    },
     entry_points={
         "console_scripts": [
             "eyewear-counter-app=app.app:main",
         ],
     },
     author="Ekaterina Solovyeva",
+    author_email="qksolov@gmail.com",
     description="Fast model for counting eyewear types in large sets of images.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/qksolov/eyewear-counter",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.8",
 )
